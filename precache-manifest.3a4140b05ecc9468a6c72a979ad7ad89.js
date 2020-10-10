91d6146fa7fe0ddbc14e54b42286dfde6ec70245
Joe Maddalone <joe.maddalone@gmail.com>
2020-10-09 19:59:29 -0500

    Updates

--- precache-manifest.3a4140b05ecc9468a6c72a979ad7ad89.js
+++ precache-manifest.3a4140b05ecc9468a6c72a979ad7ad89.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f2f510a91d0622bc8da939fee93e2fed",
+    "revision": "084f306b940a066511bc27116a762e25",
     "url": "/react-svg-path/index.html"
   },
   {
-    "revision": "a9ee22f13ef1a2fb683e",
+    "revision": "481ae8268ab89d9be4d3",
     "url": "/react-svg-path/static/css/main.ead9e477.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-svg-path/static/js/2.7bbecc49.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a9ee22f13ef1a2fb683e",
-    "url": "/react-svg-path/static/js/main.45e399f1.chunk.js"
+    "revision": "481ae8268ab89d9be4d3",
+    "url": "/react-svg-path/static/js/main.aa819fbd.chunk.js"
   },
   {
     "revision": "ca62c705fde03a4fcee0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-svg-path/precache-manifest.ad17fff6634e6d332fa265fde042825e.js"
+  "/react-svg-path/precache-manifest.3a4140b05ecc9468a6c72a979ad7ad89.js"
 );
 
 self.addEventListener('message', (event) => {
