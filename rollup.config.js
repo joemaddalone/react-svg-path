2bb0da030e4e04c3a6a6ecd1bf0cd2c1051c9377
Joe Maddalone <>
2020-10-25 14:00:51 -0500

    eject from crl

---
+++ rollup.config.js
@@ -0,0 +1,58 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
+
+const INPUT_FILE_PATH = 'src/index.js';
+const OUTPUT_NAME = 'react-svg-path';
+
+const GLOBALS = {
+  react: 'React',
+  'react-dom': 'ReactDOM'
+};
+
+const EXTERNAL = ['react', 'react-dom'];
+
+const PLUGINS = [
+  babel({
+    babelHelpers: 'runtime',
+    exclude: 'node_modules/**'
+  }),
+  resolve({
+    browser: true,
+    resolveOnly: [/^(?!react$)/, /^(?!react-dom$)/, /^(?!prop-types)/]
+  }),
+  commonjs()
+];
+
+// https://github.com/rollup/plugins/tree/master/packages/babel#babelhelpers
+const CJS_AND_ES_EXTERNALS = EXTERNAL.concat(/@babel\/runtime/);
+
+const OUTPUT_DATA = [
+  {
+    file: pkg.browser,
+    format: 'umd'
+  },
+  {
+    file: pkg.main,
+    format: 'cjs'
+  },
+  {
+    file: pkg.module,
+    format: 'es'
+  }
+];
+
+const config = OUTPUT_DATA.map(({ file, format }) => ({
+  input: INPUT_FILE_PATH,
+  output: {
+    file,
+    format,
+    name: OUTPUT_NAME,
+    globals: GLOBALS
+  },
+  external: ['cjs', 'es'].includes(format) ? CJS_AND_ES_EXTERNALS : EXTERNAL,
+  plugins: PLUGINS
+}));
+
+export default config;
