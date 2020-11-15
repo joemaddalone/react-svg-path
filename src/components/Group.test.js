4546de49fc73d23fde5c86316e980334d3e08a29
Joe Maddalone <>
2020-11-15 11:38:27 -0600

    Group & Line tests

---
+++ src/components/Group.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Group } from './Group';
+
+const shapeTest = (Component) => {
+  return render(<svg>{Component}</svg>);
+};
+
+describe('<Group />', () => {
+  it('should render correctly', () => {
+    const { getByTestId } = render(
+      <svg>
+        <Group>
+          <rect data-testid='child' />
+        </Group>
+      </svg>
+    );
+    expect(getByTestId('child')).toBeDefined();
+  });
+});
