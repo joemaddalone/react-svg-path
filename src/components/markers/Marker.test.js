5b55a32039c0f3a85784975132ab78657558fd30
Joe Maddalone <>
2020-11-22 23:40:39 -0600

    test(markers): adds test for Marker components
    Add Marker tests and updates travis config to only run release on master.

---
+++ src/components/markers/Marker.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Marker } from './Marker';
+import { MarkerArrow } from './MarkerArrow';
+import { MarkerLine } from './MarkerLine';
+import { MarkerSquare } from './MarkerSquare';
+import { MarkerTriangle } from './MarkerTriangle';
+
+describe('<Marker />', () => {
+  it('should render correctly', () => {
+    const { getByTestId, container } = render(
+      <svg>
+        <Marker
+          data-testid='m'
+          id='x'
+          markerWidth={10}
+          markerHeight={11}
+          refX={8}
+          refY={5}
+        >
+          <circle cx={0} cy={0} r={10} data-testid='c' />
+        </Marker>
+      </svg>
+    );
+    expect(container.querySelector('def')).toBeDefined();
+    expect(getByTestId('c')).toBeDefined();
+  });
+});
+
+describe('Markers', () => {
+  [
+    { C: MarkerArrow, name: 'MarkerArrow' },
+    { C: MarkerLine, name: 'MarkerLine' },
+    { C: MarkerSquare, name: 'MarkerSquare' },
+    { C: MarkerTriangle, name: 'MarkerTriangle' }
+  ].forEach(({ C, name }) => {
+    it(`${name} renders correctly`, () => {
+      const { getByTestId, container } = render(
+        <svg>
+          <C data-testid='m' />
+        </svg>
+      );
+      expect(container.querySelector('def')).toBeDefined();
+      expect(getByTestId('m')).toBeDefined();
+    });
+  });
+});
