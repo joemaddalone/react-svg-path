5459b2a4383800f2d2b2a7728cf5bfffa364522a
Joe Maddalone <>
2020-11-27 12:56:43 -0600

    fix(markersquare): markerSquare relative points crashes on mid-marker

--- src/components/markers/MarkerSquare.js
+++ src/components/markers/MarkerSquare.js
@@ -2,23 +2,28 @@ import React from 'react';
 import { Marker } from './Marker';
 import { Polyline } from '../Polyline';
 
-export const MarkerSquare = ({ id, size = 10, ...attributes }) => {
+export const MarkerSquare = ({
+  id,
+  size = 10,
+  color = '#000',
+  ...attributes
+}) => {
   return (
     <Marker
       id={id}
       markerWidth={size * 2}
       markerHeight={size * 2}
-      refX={0}
+      refX={size / 2}
       refY={size / 2}
     >
       <Polyline
         points={[
           [0, 0],
           [size, 0],
-          [0, size],
-          [-size, 0]
+          [size, size],
+          [0, size]
         ]}
-        relative
+        fill={color}
         {...attributes}
       />
     </Marker>
