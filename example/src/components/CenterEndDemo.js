916e54ac8f4d7a68049da1ed880b926ebffa3430
Joe Maddalone <>
2020-11-27 13:26:23 -0600

    docs(docs): add docs for centerEnd

--- example/src/components/CenterEndDemo.js
+++ example/src/components/CenterEndDemo.js
@@ -1,31 +1,100 @@
 import React from 'react';
-import { Svg, RadialLines, MarkerLine } from 'react-svg-path';
+import {
+  Svg,
+  Square,
+  Circle,
+  Text,
+  Line,
+  MarkerSquare,
+  MarkerArrow
+} from 'react-svg-path';
 
 export const CenterEndDemo = () => {
   return (
     <>
       <h2>centerEnd</h2>
       <p>
-        Some components in react-svg-path are guranteed to end at their center
+        Some components in react-svg-path are guaranteed to end at their center
         points. This behavior can be turned off with{' '}
-        <i>centerEnd={`{false}`}</i>
+        <i>centerEnd={`{false}`}</i>. Nested components will still recieve the
+        center x & y from parents.
       </p>
-      <Svg width={500} height={500}>
-        <MarkerLine id='markers' size={40} strokeWidth={3} />
-        <RadialLines
-          fill='none'
-          cx={250}
-          cy={250}
-          innerSize={200}
-          outerSize={250}
-          points={16}
-          stroke='#222'
-          centerEnd={false}
-          markerMid='url(#markers)'
-          markerStart='url(#markers)'
-          markerEnd='url(#markers)'
-        />
-      </Svg>
+      <div>
+        <Svg width={575} height={175}>
+          <MarkerSquare id='marker-s' size={10} color='#999' />
+          <MarkerArrow id='marker-a' size={10} color='red' />
+          <Square
+            fill='none'
+            cx={250}
+            cy={100}
+            size={75}
+            stroke='#222'
+            centerEnd={false}
+            markerEnd='url(#marker-s)'
+          >
+            <Circle size={10}>
+              <Line
+                ox={15}
+                ex={75}
+                ey={0}
+                relative
+                stroke='red'
+                markerStart='url(#marker-a)'
+              />
+              <Text ox={100} oy={4}>
+                Circle still inherits center cx & cy
+              </Text>
+            </Circle>
+          </Square>
+          <Line
+            sx={150}
+            sy={60}
+            ex={200}
+            ey={60}
+            stroke='red'
+            markerEnd='url(#marker-a)'
+          >
+            <Text
+              ox={-140}
+              oy={4}
+              attach='start'
+            >Square new end x & y</Text>
+          </Line>
+        </Svg>
+      </div>
+      <div>
+        <code>
+          {`
+<Svg width={575} height={175}>
+<MarkerSquare id='marker-s' size={10} color='#999' />
+<MarkerArrow id='marker-a' size={10} color='red' />
+<Square
+  fill='none'
+  cx={250}
+  cy={100}
+  size={75}
+  stroke='#222'
+  centerEnd={false}
+  markerEnd='url(#marker-s)'
+>
+  <Circle size={10}>
+    <Line ox={15} ex={75} ey={0} relative stroke="red" markerStart='url(#marker-a)' />
+    <Text ox={100} oy={4}>Circle still inherits center cx & cy</Text>
+  </Circle>
+</Square>
+<Line
+  sx={150}
+  sy={60}
+  ex={200}
+  ey={60}
+  stroke='red'
+  markerEnd='url(#marker-a)'
+>
+  <Text ox={-140} oy={4} attach='start'>Square new end x & y</Text>
+</Line>
+</Svg>        `.trim()}
+        </code>
+      </div>
     </>
   );
 };
