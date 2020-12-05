98f167f1c67a4d801b530116c5dd0518c1aa4db5
Joe Maddalone <>
2020-12-05 11:02:31 -0600

    fix(attributes): remove invalid attributes from rendered paths
    closes #43

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import cleanAttributes from '../utils/cleanAttributes';
 
 export const Text = ({ x, y, sx, sy, children, ...attributes }) => {
-  const allowedAttributes = { ...attributes };
-  delete allowedAttributes.sx;
-  delete allowedAttributes.sy;
-  delete allowedAttributes.cx;
-  delete allowedAttributes.cy;
+  const allowedAttributes = cleanAttributes(attributes);
   return (
     <text x={sx || x} y={sy || y} {...allowedAttributes}>
       {children}
--- src/utils/render.js
+++ src/utils/render.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import attach from './attach';
+import cleanAttributes from './cleanAttributes';
 
 const render = ({ pathMethod, attributes, ex, ey, sx, sy, children }) => {
   // eslint-disable-next-line no-unused-vars
   const { merge, attach: attachProp, ...restOfComponentProps } = attributes;
   const to = merge ? 'toString' : 'toComponent';
+  const cleanComponentProps = cleanAttributes(restOfComponentProps);
   if (!children) {
-    return pathMethod ? pathMethod()[to](restOfComponentProps) : null;
+    return pathMethod ? pathMethod()[to](cleanComponentProps) : null;
   } else {
     return [
-      pathMethod
-        ? pathMethod()[to]({ ...restOfComponentProps, key: -1 })
-        : null,
+      pathMethod ? pathMethod()[to]({ ...cleanComponentProps, key: -1 }) : null,
       React.Children.map(children, (child, i) =>
         React.cloneElement(child, {
           key: i,
