3ddb90d3d90d3943f047c9da3c9ecb8f196adf7c
Joe Maddalone <>
2020-12-13 10:31:13 -0600

    docs(bars-demo): clean up d3 usage

--- example/src/components/demos/complex/Bars.js
+++ example/src/components/demos/complex/Bars.js
@@ -1,5 +1,5 @@
-import { Svg, Line, Text } from "react-svg-path";
-import { scaleLinear } from "d3-scale";
+import { Svg, Line, Text } from 'react-svg-path';
+import { scaleLinear } from 'd3-scale';
 
 const values = [
   { val: 200 },
@@ -10,34 +10,33 @@ const values = [
   { val: 150 },
   { val: 120 },
   { val: 340 },
-  { val: 276 },
+  { val: 200 },
   { val: 249 },
   { val: 90 },
   { val: 78 },
-  { val: 174 }
-
+  { val: 125 }
 ];
 const max = Math.max.apply(
   Math,
-  values.map( (o) => o.val)
+  values.map((o) => o.val)
 );
 
 const width = 680;
 const margin = 75;
-const height = max + margin;
+const height = 400;
 
 const yScale = scaleLinear()
-  .domain([0, height])
-  .range([height, 0]);
+  .domain([0, max])
+  .range([0, height]).nice();
 
 const Bars = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <Svg width={width + margin} height={height + margin}>
         <g transform={`translate(${margin / 2}, ${margin / 2})`}>
           <Line
             strokeWidth={0.5}
-            stroke="#ccc"
+            stroke='#ccc'
             sx={25}
             sy={0}
             ex={25}
@@ -48,37 +47,38 @@ const Bars = () => {
               <g key={y}>
                 <Line
                   strokeWidth={0.5}
-                  stroke="#888"
+                  stroke='#888'
                   sx={0}
                   sy={height - y}
                   ex={width}
                   ey={height - y}
                 >
-                  <Text fontSize="0.75em" oy={-2} sx={0} textAnchor="start">
+                  <Text fontSize='0.75em' oy={-2} sx={0} textAnchor='start'>
                     {y}
                   </Text>
                 </Line>
               </g>
             );
           })}
-          {values.map(({ val, label }, index, arr) => {
+          {values.map(({ val }, index, arr) => {
             const barCount = arr.length;
             const xStart = 50;
             const xSpace = width - xStart;
             const barWidth = xSpace / (barCount * 2);
             const x = barWidth * index * 2;
+            const label = String.fromCharCode(index + 65);
             return (
               <g transform={`translate(${xStart}, 0)`} key={index}>
                 <Line
                   sx={x}
-                  sy={yScale(val)}
-                  ex={x}
                   ey={height}
+                  ex={x}
+                  sy={yScale(height) - yScale(val)}
                   strokeWidth={barWidth}
-                  stroke="rebeccapurple"
+                  stroke='rebeccapurple'
                 />
-                <Text fontSize="0.75em" x={x - 5} y={height + 20}>
-                  {String.fromCharCode(index + 65)}
+                <Text fontSize='0.75em' x={x - 5} y={height + 20}>
+                  {label}
                 </Text>
               </g>
             );
