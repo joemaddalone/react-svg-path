500e213f39e601ce06a42471039d4f39c8f368cb
Joe Maddalone <>
2021-03-06 11:22:04 -0600

    fix(pathmerge): allow custom path elements to be merged
    issue #69

--- example/src/TestApp.js
+++ example/src/TestApp.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import { Svg, Line, Circle } from 'react-svg-path';
+import Path, {
+  Svg,
+  Circle,
+  PathMerge,
+  RegPolygon,
+  Square,
+  Line
+} from 'react-svg-path';
 
 import './App.css';
+const PathOne = () => <path d='M0,0h100v100' stroke='red' />;
+const PathTwo = () => <path d='M0,0v100h200' stroke='green' />;
+const PathThree = ({ x, y, w, h, ...rest }) => {
+  const p = new Path();
+  return p
+    .M(x, y)
+    .l(-w / 2, h)
+    .right(w)
+    .close()
+    .toComponent(rest);
+};
 
 const App = () => {
   return (
-    <div>
-      <Svg width={500} height={500}>
-        <Line exy={10} sxy={490} stroke='#333'>
-          <Circle size={10} stroke='#333' />
-        </Line>
+    <>
+      <Svg width={300} height={300}>
+        <PathMerge fill='none' stroke='green'>
+          <Circle size={10} />
+          <PathOne />
+          <PathTwo />
+          <PathThree x={100} y={100} w={150} h={150} />
+        </PathMerge>
       </Svg>
-    </div>
+      <Svg width={200} height={200} className='nesting-example-1' scale>
+        <PathMerge fill='none' stroke='black'>
+          <RegPolygon size={120} sides={4} cx={100} cy={100}>
+            <Circle size={55} />
+            <Square size={60} />
+            <Circle size={25} cy={58} />
+          </RegPolygon>
+          <Circle size={10} cx={100} cy={100} />
+        </PathMerge>
+      </Svg>
+      <svg width={250} height={250}>
+        <PathMerge fill='none' stroke='black'>
+          <Line sx={0} sy={0} ex={100} ey={100}>
+            <Line sx={50} sy={80} ex={300} ey={300} />
+          </Line>
+          <path d='M0,0h200v100' stroke='red' />
+        </PathMerge>
+      </svg>
+    </>
   );
 };
 
